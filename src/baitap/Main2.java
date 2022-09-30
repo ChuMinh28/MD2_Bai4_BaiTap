1cc02c06fa2877072ac617fe705c3a559f37049c
ChuMinh28 <quangminh2872000@gmail.com>
2022-09-30 09:06:47 +0700

    add

--- src/baitap/Main2.java
+++ src/baitap/Main2.java
@@ -1,15 +1,28 @@
 package baitap;
 
-import java.util.Date;
-
 public class Main2 {
     public static void main(String[] args) {
-        Date currentDate = new Date();
-        System.out.println(currentDate);
-        long time = currentDate.getTime();
-        long time2 = System.currentTimeMillis();
-
-        System.out.println(time);
-        System.out.println(time2);
+        // Khoi tao array co 10000 phan tu
+        int[] array = new int[10000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)Math.random()*10000;
+        }
+        // Start dong ho de bat dau dem thoi gian giai thuat
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        // Thuc hien giai thuat
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = i+1; j <array.length; j++) {
+                if (array[i]>array[j]){
+                    int x = array[i];
+                    array[i] = array[j];
+                    array[j] = x;
+                }
+            }
+        }
+        //Stop dong ho de tinh so thoi gian thuc hien giai thuat
+        stopWatch.stop();
+        //In so thoi gian thuc hien giai thuat
+        System.out.println("thoi gian thuc hien giai thuat la: "+stopWatch.getElapsedTime());
     }
 }
---
+++ src/baitap/StopWatch.java
@@ -0,0 +1,37 @@
+package baitap;
+
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(long endTime) {
+        this.endTime = endTime;
+    }
+
+    public StopWatch() {
+        this.startTime = System.currentTimeMillis();
+    }
+    public void start(){
+        this.startTime = System.currentTimeMillis();
+    }
+    public void stop(){
+        this.endTime = System.currentTimeMillis();
+    }
+
+    public long getElapsedTime(){
+        long time = this.endTime - this.startTime;
+        return time;
+    }
+}
